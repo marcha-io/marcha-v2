99b5566ce3ec1494e59ca4a7eed3c71933ecdd46
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2023-09-21 14:46:21 +0100

    added talwind config

---
+++ app/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    './src/pages/*.tsx',
+    './src/**/*.tsx',
+    './public/stylesheets/**/*.css',
+  ],
+  plugins: [
+    require('flowbite/plugin'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        marcha: '#F65E44',
+        marchaDark: '#dd553d',
+        grey: '#f9f9f9',
+        transparent: 'transparent',
+        current: 'currentColor',
+      },
+    },
+  },
+}
